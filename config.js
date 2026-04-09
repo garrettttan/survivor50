8d38dd848041761707614e4b8cbc8de7b968ffbd
garrettttan <garrett.t.tan@gmail.com>
2026-04-09 14:33:17 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   SHEET_ID: "1PkLIIdK8ky24acRPV2rVqx7MHruTMMD0mw_U5gokl6M",
 
   SEASONS: [
-    { title: "Season 50", mainSheet: "Season 50" },
+    { title: "Season 50", mainSheet: "Season 50", scoringSheet: "Season 50 Scoring" },
     { title: "Season 49", mainSheet: "Season 49" },
     { title: "Season 48", mainSheet: "Season 48" },
   ],
